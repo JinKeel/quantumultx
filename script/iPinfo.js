09ec06692fa60404e86ae547c45bb70e11a6071c
Z <z-jinke@qq.com>
2026-06-23 12:22:43 +0800

    Update title and error message in iPinfo script

--- script/iPinfo.js
+++ script/iPinfo.js
@@ -2,16 +2,16 @@ const nodeName = $environment.params;
 
 $task.fetch({
     url: "http://ipwho.is/?lang=zh-CN",
-    opts: {
-        policy: nodeName
+    opts: { policy: nodeName
     }
 }).then(resp => {
     const ipwho = JSON.parse(resp.body);
   
     const html = `
-<div style="text-align:left;font-family:-apple-system;line-height:1.9;padding-left:10px;">
-<span style="font-size:19px;font-weight:600;color:#1599FF;font-family:Menlo;">
-${ipwho.type}:${ipwho.ip}
+<div style="text-align:left;font-family:-apple-system;line-height:1.5;">
+
+<span style="font-size:19px;font-weight:650;color:#439DF8;">
+${ipwho.type} ${ipwho.ip}
 </span><br><br>
 
 <span style="font-size:15px;">
@@ -23,7 +23,7 @@ ${ipwho.type}:${ipwho.ip}
 </span>`;
 
     $done({
-        title: "查询结果",
+        title: "iP查询",
         htmlMessage: html
     });
 
@@ -31,7 +31,7 @@ ${ipwho.type}:${ipwho.ip}
 
     $done({
         title: "查询超时",
-        message: `节点「${nodeName}」连接检测超时请稍后再试`
+        message: `节点「${nodeName}」连接超时请稍后再试`
     });
 
 });

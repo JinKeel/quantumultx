01884d585895aad40b8aa86d6f68ff48f6b47632
Z <z-jinke@qq.com>
2026-06-24 00:11:11 +0800

    Change IP information API from ipwho.is to ip-api.com

--- script/iPinfo.js
+++ script/iPinfo.js
@@ -1,34 +1,34 @@
 const nodeName = $environment.params;
 
 $task.fetch({
-    url: "http://ipwho.is/?lang=zh-CN",
+    url: "http://ip-api.com/json/?lang=zh-CN",
     opts: { policy: nodeName }
 }).then(resp => {
-    const ipwho = JSON.parse(resp.body);
-
+    
+    const data = JSON.parse(resp.body);
     const html = `
 <div style="text-align:center;font-family:-apple-system;line-height:1.2;"><br>
 
 <span style="font-size:18px;font-weight:650;color:#7987F2;">
-IP ${ipwho.ip}
+IP ${data.query}
 </span><br>
 
 <span style="color:#AAA;">─</span><br>
 
 <span style="font-size:14px;font-weight:500;">
-落地 ${ipwho.country} ${ipwho.country_code}
+位置${data.country} ${data.countryCode} ${data.regionName}
 </span><br>
 
 <span style="color:#AAA;">─</span><br>
 
 <span style="font-size:14px;font-weight:500;">
-运营 ${ipwho.connection.isp}
+运营${data.isp}
 </span><br>
 
 <span style="color:#AAA;">─</span><br>
 
 <span style="font-size:14px;font-weight:500;">
-策略 ${nodeName}
+策略${nodeName}
 </span>
 
 </div>`;
@@ -38,7 +38,7 @@ IP ${ipwho.ip}
         htmlMessage: html
     });
 
-}).catch(() => {
+}).catch(err => {
 
     $done({
         title: "查询超时",

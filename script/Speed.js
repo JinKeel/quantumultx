95009e2b88ab181101e0de9f8540a67c460348ee
Z <z-jinke@qq.com>
2026-06-29 10:40:33 +0800

    Refactor Mbps color coding and HTML structure
    Update color coding for Mbps display and adjust HTML output.

--- script/Speed.js
+++ script/Speed.js
@@ -11,19 +11,27 @@ $task.fetch({
     var ms = Date.now() - Time;
     var ss = ms / 1000;
     var Mbps = (15728640 * 8 / 1000000 / ss).toFixed(2);
-    
+
+    var color = "#FF3B30";
+    if (Mbps >= 500) {
+        color = "#00C853";
+    } else if (Mbps >= 200) {
+        color = "#34C759";
+    } else if (Mbps >= 100) {
+        color = "#007AFF";
+    } else if (Mbps >= 50) {
+        color = "#FF9500";
+    }
+
     var html = `
 <div style="text-align:center;font-family:-apple-system;line-height:1;"><br>
 
-<span style="font-size:20px;font-weight:600;color:#07C06E;">${Mbps} Mbps</span><br>
-<span style="color:#A0A0A0;">─</span><br>
+<span style="font-size:25px;font-weight:600;color:${color};">${Mbps} Mbps</span><br>
+<span style="color:#999;">──</span><br>
 
-<span style="font-size:15px;font-weight:500;">测速耗时 ${ss.toFixed(2)}秒</span><br>
-<span style="color:#A0A0A0;">─</span><br>
+<span style="font-size:15px;font-weight:500;">节点 ${nodeName}</span>
 
-<span style="font-size:15px;font-weight:500;">节点${nodeName}</span>
-
-</div>`;
+<div>`;
 
     $done({
         title: "节点测速",
